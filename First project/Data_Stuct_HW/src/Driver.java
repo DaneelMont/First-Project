c2e810347ede1889491832a38a1d85f028d71854
DaneelMont <55253130+DaneelMont@users.noreply.github.com>
2019-09-18 16:35:00 -0500

    Update Driver.java

--- First project/Data_Stuct_HW/src/Driver.java
+++ First project/Data_Stuct_HW/src/Driver.java
@@ -1,19 +1,40 @@
+
 public class Driver {
 
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-IterationMathlib interationLibrary = new IterationMathlib();
-RecursiveMathlib recursionLibrary = new RecursiveMathlib();
-System.out.println("----Iteration------");
-runTestCode(interationLibrary);
-System.out.println("-----Recursion------");
-runTestCode(recursionLibrary);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	House[] house1 =new House[3];
+		Scanner input=new Scanner(System.in);
+		for(int i=0;i<3;i++) {
+			Photo photo = new Photo();
+			House house1=new House();
+			System.out.println("Enter id: ");
+			house1.setId(input.nextInt());
 
-}
+			System.out.println("Enter price: ");
+			house1.setPrice(input.nextFloat());
 
-public static void runTestCode(Mathlib library) {
-System.out.println("GCD = " +library.gcd(4, 2));
-System.out.println("Fib = " +library.fib(2));
-System.out.println("Hanoi = " +library.hanoi(2));
-}
+			System.out.println("Enter location: ");
+			house1.setLocation(input.nextLine());
+
+			System.out.println("Enter advertiser: ");
+			house1.setAdvertiser(input.nextLine());
+
+			System.out.println("Enter pixel: );
+			photo.setPixel(input.nextFloat());
+
+			System.out.println("Enter byte: );
+			photo.setByte(input.nextFloat());
+
+			input.nextLine();
+			house.setPhoto(photo);
+			house[i] =house1;
+
+		}
+		for(House house1:house){
+			System.out.println(house);
+		}
+	}
+
+
 }
